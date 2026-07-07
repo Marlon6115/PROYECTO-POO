693f0be98bd8f7f211f98d779337ecfd644dd20f
Administrator <marlon.casagallo@epn.edu.ec>
2026-07-06 20:07:11 -0500

    Correcion de errores de version maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.example.proyectopoo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens org.example.proyectopoo to javafx.fxml;
     exports org.example.proyectopoo;
 }
