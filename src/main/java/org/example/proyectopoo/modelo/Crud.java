9ee18c147735cede27833e11fd1c0292ee0083a9
DannySalcedopol <dannysalcedo0112@gmail.com>
2026-07-09 17:33:46 -0500

    (modelo): interfaz Crud y clases Estudiante y Usuario completadas

--- src/main/java/org/example/proyectopoo/modelo/Crud.java
+++ src/main/java/org/example/proyectopoo/modelo/Crud.java
@@ -1,4 +1,17 @@
 package org.example.proyectopoo.modelo;
 
+import java.util.Map;
+
 public interface Crud {
+
+    public Map<Integer, Estudiante> seleccionarTodo();
+
+    public Estudiante buscar(int id);
+
+    public void insertar(Estudiante estudiante);
+
+    public void actualizar(Estudiante estudiante);
+
+    public void eliminar(int id);
+
 }
